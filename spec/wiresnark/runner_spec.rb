833fcfcb99d629e5fe8e8a06ecbf7637d7f3fc7e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-21 13:34:29 +0200

    Runner spec: update for better minispec 2.3+ mocks

--- spec/wiresnark/runner_spec.rb
+++ spec/wiresnark/runner_spec.rb
@@ -3,9 +3,9 @@ require_relative '../spec_helper'
 module Wiresnark module Runner
 
   describe '.run' do
-    it 'evaluates the file in the context of the DSL object' do
+    it 'evaluates the contents of the file in the context of the passed object' do
       env = MiniTest::Mock.new
-      env.expect :send_to, nil, ['lo']
+      env.expect :instance_eval, nil, [File.read('spec/fixtures/seven-to-lo.rb')]
       Runner.run 'spec/fixtures/seven-to-lo.rb', env
       env.verify
     end
