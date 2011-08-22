457532eae7135a1e88ee1dbf43351daf8a84da11
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-22 14:36:17 +0200

    sane defaults

--- lib/wiresnark/executable.rb
+++ lib/wiresnark/executable.rb
@@ -1,10 +1,10 @@
 module Wiresnark class Executable
 
-  def initialize files
+  def initialize files = ARGV
     @files = files
   end
 
-  def run runner
+  def run runner = Runner
     runner.run @files.first
   end
 
--- lib/wiresnark/runner.rb
+++ lib/wiresnark/runner.rb
@@ -1,6 +1,6 @@
 module Wiresnark module Runner
 
-  def self.run file, env
+  def self.run file, env = Object.new.extend(DSL::WiresnarkDSL)
     env.instance_eval File.read file
   end
 
