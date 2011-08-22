647d0fc7eeac6ed51543826195f4f10f638bff68
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-22 11:21:23 +0200

    make Interface.new attach to the interface

--- lib/wiresnark/interface.rb
+++ lib/wiresnark/interface.rb
@@ -1,8 +1,12 @@
 module Wiresnark class Interface
 
-  def self.new name
+  def self.new name, pcap = nil
     @interfaces       ||= {}
     @interfaces[name] ||= super
+  end
+
+  def initialize name, pcap
+    @stream = pcap.open_live(name, 0xffff, true, 0) if pcap.respond_to? :open_live
   end
 
 end end
--- spec/wiresnark/interface_spec.rb
+++ spec/wiresnark/interface_spec.rb
@@ -7,6 +7,13 @@ module Wiresnark describe Interface do
       Interface.new('foo').must_be_same_as Interface.new 'foo'
       Interface.new('foo').wont_be_same_as Interface.new 'bar'
     end
+
+    it 'hooks to the inteface using the provided interface' do
+      pcap = MiniTest::Mock.new
+      pcap.expect :open_live, nil, ['lo', 0xffff, true, 0]
+      Interface.new 'lo', pcap
+      pcap.verify
+    end
   end
 
 end end
