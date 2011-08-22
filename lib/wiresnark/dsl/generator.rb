6c5f9a8d83ce2759354e096e3b6c6a43f397f223
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-22 06:50:40 +0200

    add DSL::Generator#count

--- lib/wiresnark/dsl/generator.rb
+++ lib/wiresnark/dsl/generator.rb
@@ -2,4 +2,8 @@ module Wiresnark module DSL::Generator
 
   include DSL::Common
 
+  def count count = nil
+    count.nil? ? @count : @count = count
+  end
+
 end end
--- spec/wiresnark/dsl/generator_spec.rb
+++ spec/wiresnark/dsl/generator_spec.rb
@@ -1,4 +1,14 @@
 require_relative '../../spec_helper'
 
 module Wiresnark describe DSL::Generator do
+
+  describe '#count' do
+    it 'stores the passed count' do
+      env = Object.new.extend DSL::Generator
+      env.count.must_be_nil
+      env.count 7
+      env.count.must_equal 7
+    end
+  end
+
 end end
