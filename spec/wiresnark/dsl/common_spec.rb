f8847aea56e738ba046beffef198d624a90ce3d9
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-22 07:06:28 +0200

    refactor DSL::Common spec

--- spec/wiresnark/dsl/common_spec.rb
+++ spec/wiresnark/dsl/common_spec.rb
@@ -2,21 +2,23 @@ require_relative '../../spec_helper'
 
 module Wiresnark describe DSL::Common do
 
+  before do
+    @env = Object.new.extend DSL::Common
+  end
+
   describe '#interface' do
     it 'stores the passed interface' do
-      env = Object.new.extend DSL::Common
-      env.interface.must_be_nil
-      env.interface 'lo'
-      env.interface.must_equal 'lo'
+      @env.interface.must_be_nil
+      @env.interface 'lo'
+      @env.interface.must_equal 'lo'
     end
   end
 
   describe '#verbose, #verbose?' do
     it 'turns on and reads the verbose flag' do
-      env = Object.new.extend DSL::Common
-      refute env.verbose?
-      env.verbose
-      assert env.verbose?
+      refute @env.verbose?
+      @env.verbose
+      assert @env.verbose?
     end
   end
 
