e2c2bcf8a8dacdcc829cd3829f54801b4c42b2d5
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-23 11:39:23 +0200

    Generator.generate: move sequenced payload one byte further

--- lib/wiresnark/generator.rb
+++ lib/wiresnark/generator.rb
@@ -2,7 +2,7 @@ module Wiresnark module Generator
 
   def self.generate env, count = env.count
     Array.new(count) do |n|
-      env.payload (n % 0x10000).to_s(16).rjust(8, '0').chars.each_slice(2).map(&:join).pack('H2H2H2H2') if env.sequence?
+      env.payload "\x00" + (n % 0x10000).to_s(16).rjust(8, '0').chars.each_slice(2).map(&:join).pack('H2H2H2H2') if env.sequence?
       Packet.new env
     end
   end
