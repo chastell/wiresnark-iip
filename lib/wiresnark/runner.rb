4bbecb30e0f629425c1d3c04bab267c1e6e4332f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-23 07:48:17 +0200

    make Runner.run send packets

--- lib/wiresnark/runner.rb
+++ lib/wiresnark/runner.rb
@@ -2,6 +2,11 @@ module Wiresnark module Runner
 
   def self.run file, env = Object.new.extend(DSL::WiresnarkDSL)
     env.instance_eval File.read file
+    env.generator_blocks.each do |block|
+      gen = Object.new.extend DSL::GeneratorDSL
+      gen.instance_eval &block
+      Interface.new(gen.interface).inject Generator.generate(gen), (gen.verbose ? $stdout : nil)
+    end
   end
 
 end end
--- spec/wiresnark/runner_spec.rb
+++ spec/wiresnark/runner_spec.rb
@@ -3,11 +3,20 @@ require_relative '../spec_helper'
 module Wiresnark module Runner
 
   describe '.run' do
-    it 'evaluates the contents of the file in the context of the passed object' do
+    it 'evaluates the passed file and sends packets as per spec' do
       env = MiniTest::Mock.new
-      env.expect :instance_eval, nil, [File.read('spec/fixtures/seven-to-lo.rb')]
+      env.expect :instance_eval,    nil, [File.read('spec/fixtures/three-to-lo.rb')]
+      env.expect :generator_blocks, []
       Runner.run 'spec/fixtures/three-to-lo.rb', env
       env.verify
+    end
+
+    it 'outputs the traffic info if so instructed' do
+      -> { Runner.run 'spec/fixtures/three-to-lo.rb' }.must_output <<-END
+-> lo\tEth\t00:00:00:00:00:00 00:00:00:00:00:00 08 00#{' 00' * 46}
+-> lo\tEth\t00:00:00:00:00:00 00:00:00:00:00:00 08 00#{' 00' * 46}
+-> lo\tEth\t00:00:00:00:00:00 00:00:00:00:00:00 08 00#{' 00' * 46}
+      END
     end
   end
 
