143ec074c6ab9a7e7d1f15f080862cc93c345b5b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-23 06:35:01 +0200

    make Generator.generate work based on an object, rather than a block

--- lib/wiresnark/generator.rb
+++ lib/wiresnark/generator.rb
@@ -1,8 +1,6 @@
 module Wiresnark module Generator
 
-  def self.generate &block
-    env = Object.new.extend DSL::GeneratorDSL
-    env.instance_eval &block
+  def self.generate env
     Array.new(env.count) { Packet.new env }
   end
 
--- spec/wiresnark/generator_spec.rb
+++ spec/wiresnark/generator_spec.rb
@@ -4,18 +4,20 @@ module Wiresnark describe Generator do
 
   describe '.generate' do
     it 'generates packages based on the passed block' do
-      Generator.generate do
-        count 3
-      end.must_equal [Packet.new, Packet.new, Packet.new]
+      env = Object.new.extend DSL::GeneratorDSL
+      env.instance_eval { count 3 }
+      Generator.generate(env).must_equal [Packet.new, Packet.new, Packet.new]
 
       packet = Packet.new "\xaa\xbb\xcc\xdd\xee\xff" + "\x11\x22\x33\x44\x55\x66" + "\x08\x00" + "\x02" + 'foo' + "\x00" * 42
-      Generator.generate do
+      env = Object.new.extend DSL::GeneratorDSL
+      env.instance_eval do
         count 5
         destination_mac 'aa:bb:cc:dd:ee:ff'
         payload 'foo'
         source_mac '11:22:33:44:55:66'
         type 'CAN'
-      end.must_equal [packet, packet, packet, packet, packet]
+      end
+      Generator.generate(env).must_equal [packet, packet, packet, packet, packet]
     end
   end
 
