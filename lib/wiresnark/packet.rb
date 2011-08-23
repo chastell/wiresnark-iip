33cdeac12ea9d4599793c7d2b1f1f0ddd8d9975b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-23 17:42:20 +0200

    Packet#to_s: add size

--- lib/wiresnark/packet.rb
+++ lib/wiresnark/packet.rb
@@ -56,7 +56,7 @@ module Wiresnark class Packet
   end
 
   def to_s
-    "#{type}\t#{destination_mac} #{source_mac} #{@bin[12..-1].unpack('H2' * (@bin.size - 12)).join ' '}"
+    "#{type}\t#{@bin.size}\t#{destination_mac} #{source_mac} #{@bin[12..-1].unpack('H2' * (@bin.size - 12)).join ' '}"
   end
 
   def type
