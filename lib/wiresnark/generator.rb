f1b8a799bceb3f6d0c1d458f9d70e7c7fc8fe296
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-09-05 01:45:39 +0200

    Generator.generate: pass a Hash to Packet.new

--- lib/wiresnark/generator.rb
+++ lib/wiresnark/generator.rb
@@ -3,7 +3,7 @@ module Wiresnark module Generator
   def self.generate env, count = env.count
     Array.new(count) do |n|
       env.payload "\x00" + [(n % 0x100000000).to_s(16).rjust(8, '0')].pack('H8') if env.sequence?
-      Packet.new env
+      Packet.new env.params
     end
   end
 
