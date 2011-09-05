9279e37483ca4e07b2611e073a182829af4283f0
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-09-05 02:05:56 +0200

    DSL::PacketDSL: define setters together

--- lib/wiresnark/dsl/packet_dsl.rb
+++ lib/wiresnark/dsl/packet_dsl.rb
@@ -1,31 +1,18 @@
 module Wiresnark module DSL::PacketDSL
 
-  def destination_mac mac
-    params[:destination_mac] = mac
-  end
-
-  def min_size size
-    params[:min_size] = size
+  [
+    :destination_mac,
+    :min_size,
+    :payload,
+    :random_size,
+    :source_mac,
+    :type,
+  ].each do |name|
+    define_method(name) { |value| params[name] = value }
   end
 
   def params
     @params ||= {}
-  end
-
-  def payload payload
-    params[:payload] = payload
-  end
-
-  def random_size range
-    params[:random_size] = range
-  end
-
-  def source_mac mac
-    params[:source_mac] = mac
-  end
-
-  def type type
-    params[:type] = type
   end
 
 end end
