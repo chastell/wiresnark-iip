6ad0995fa5576521c890648e5a00ce642a36f69f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-09-05 02:03:15 +0200

    drop DSL::PacketDSL getters

--- lib/wiresnark/dsl/packet_dsl.rb
+++ lib/wiresnark/dsl/packet_dsl.rb
@@ -1,31 +1,31 @@
 module Wiresnark module DSL::PacketDSL
 
-  def destination_mac mac = nil
-    mac.nil? ? params[:destination_mac] : params[:destination_mac] = mac
+  def destination_mac mac
+    params[:destination_mac] = mac
   end
 
-  def min_size size = nil
-    size.nil? ? params[:min_size] || 60 : params[:min_size] = size
+  def min_size size
+    params[:min_size] = size
   end
 
   def params
     @params ||= {}
   end
 
-  def payload payload = nil
-    payload.nil? ? params[:payload] : params[:payload] = payload
+  def payload payload
+    params[:payload] = payload
   end
 
-  def random_size range = nil
-    range.nil? ? params[:random_size] : params[:random_size] = range
+  def random_size range
+    params[:random_size] = range
   end
 
-  def source_mac mac = nil
-    mac.nil? ? params[:source_mac] : params[:source_mac] = mac
+  def source_mac mac
+    params[:source_mac] = mac
   end
 
-  def type type = nil
-    type.nil? ? params[:type] : params[:type] = type
+  def type type
+    params[:type] = type
   end
 
 end end
