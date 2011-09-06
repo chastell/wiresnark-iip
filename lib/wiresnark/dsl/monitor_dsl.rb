65b5e9e50220838c542c7c0637c4fc361113646d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-09-06 14:06:37 +0200

    add DSL::MonitorDSL#running_tally and #running_tally?

--- lib/wiresnark/dsl/monitor_dsl.rb
+++ lib/wiresnark/dsl/monitor_dsl.rb
@@ -2,4 +2,12 @@ module Wiresnark module DSL::MonitorDSL
 
   include DSL::CommonDSL
 
+  def running_tally
+    @running_tally = true
+  end
+
+  def running_tally?
+    @running_tally
+  end
+
 end end
--- spec/wiresnark/dsl/monitor_dsl_spec.rb
+++ spec/wiresnark/dsl/monitor_dsl_spec.rb
@@ -1,4 +1,15 @@
 require_relative '../../spec_helper'
 
 module Wiresnark describe DSL::MonitorDSL do
+
+  describe '#running_tally, #running_tally?' do
+    it 'turns on and reads the running_tally flag' do
+      env = Object.new.extend DSL::MonitorDSL
+
+      refute env.running_tally?
+      env.running_tally
+      assert env.running_tally?
+    end
+  end
+
 end end
