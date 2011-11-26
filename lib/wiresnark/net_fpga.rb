ddba183090c8d40e4f8dbf8cf33d4497dbdb2c28
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-11-26 12:32:50 +0100

    add NetFPGA#get

--- lib/wiresnark/net_fpga.rb
+++ lib/wiresnark/net_fpga.rb
@@ -1,7 +1,12 @@
 module Wiresnark class NetFPGA
   attr_reader :registers
 
-  def initialize reg_path
+  def initialize reg_path, regbridge = nil
+    @regbridge = regbridge
     @registers = RegParser.parse reg_path
+  end
+
+  def get register
+    @regbridge.get @registers[register]
   end
 end end
--- spec/wiresnark/net_fpga_spec.rb
+++ spec/wiresnark/net_fpga_spec.rb
@@ -3,6 +3,16 @@
 require_relative '../spec_helper'
 
 module Wiresnark describe NetFPGA do
+  describe '#get' do
+    it 'gets the register contents (via the passed bridge)' do
+      regbridge = MiniTest::Mock.new
+      regbridge.expect :get, 7, [0x2000200]
+      nf = NetFPGA.new 'spec/fixtures/reg_defines_simple_system_iip.h', regbridge
+      nf.get('SCHEDULER_0_NUM_PHASES_REG').must_equal 7
+      regbridge.verify
+    end
+  end
+
   describe '#registers' do
     it 'returns the given NetFPGA’s register map' do
       nf = NetFPGA.new 'spec/fixtures/reg_defines_simple_system_iip.h'
