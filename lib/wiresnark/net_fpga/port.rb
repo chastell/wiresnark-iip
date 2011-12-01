8e4e94a6398fc1352e60d4602f5d43cfbedf3ea5
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-01 11:14:32 +0100

    factor out NetFPGA::Port#mac

--- lib/wiresnark/net_fpga/port.rb
+++ lib/wiresnark/net_fpga/port.rb
@@ -5,15 +5,18 @@ module Wiresnark class NetFPGA::Port
   end
 
   def local_mac
-    hi = @net_fpga.get "MAC_RXTX_#{@number}_LOCAL_MAC_HI_REG"
-    lo = @net_fpga.get "MAC_RXTX_#{@number}_LOCAL_MAC_LO_REG"
-    ints = [(hi & 0xff00) >> 8, hi & 0xff, (lo & 0xff000000) >> 24, (lo & 0xff0000) >> 16, (lo & 0xff00) >> 8, lo & 0xff]
-    ints.map { |i| i.to_s(16).rjust 2, '0'}.join ':'
+    mac :local
   end
 
   def other_mac
-    hi = @net_fpga.get "MAC_RXTX_#{@number}_OTHER_MAC_HI_REG"
-    lo = @net_fpga.get "MAC_RXTX_#{@number}_OTHER_MAC_LO_REG"
+    mac :other
+  end
+
+  private
+
+  def mac locality
+    hi = @net_fpga.get "MAC_RXTX_#{@number}_#{locality.upcase}_MAC_HI_REG"
+    lo = @net_fpga.get "MAC_RXTX_#{@number}_#{locality.upcase}_MAC_LO_REG"
     ints = [(hi & 0xff00) >> 8, hi & 0xff, (lo & 0xff000000) >> 24, (lo & 0xff0000) >> 16, (lo & 0xff00) >> 8, lo & 0xff]
     ints.map { |i| i.to_s(16).rjust 2, '0'}.join ':'
   end
