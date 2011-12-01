1906dbb61b106bb389a67930a1bc6ad4c1b8621e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-01 11:08:55 +0100

    add NetFPGA::Port#other_mac

--- lib/wiresnark/net_fpga/port.rb
+++ lib/wiresnark/net_fpga/port.rb
@@ -10,4 +10,11 @@ module Wiresnark class NetFPGA::Port
     ints = [(hi & 0xff00) >> 8, hi & 0xff, (lo & 0xff000000) >> 24, (lo & 0xff0000) >> 16, (lo & 0xff00) >> 8, lo & 0xff]
     ints.map { |i| i.to_s(16).rjust 2, '0'}.join ':'
   end
+
+  def other_mac
+    hi = @net_fpga.get "MAC_RXTX_#{@number}_OTHER_MAC_HI_REG"
+    lo = @net_fpga.get "MAC_RXTX_#{@number}_OTHER_MAC_LO_REG"
+    ints = [(hi & 0xff00) >> 8, hi & 0xff, (lo & 0xff000000) >> 24, (lo & 0xff0000) >> 16, (lo & 0xff00) >> 8, lo & 0xff]
+    ints.map { |i| i.to_s(16).rjust 2, '0'}.join ':'
+  end
 end end
--- spec/wiresnark/net_fpga/port_spec.rb
+++ spec/wiresnark/net_fpga/port_spec.rb
@@ -14,4 +14,18 @@ module Wiresnark describe NetFPGA::Port do
       port.local_mac.must_equal 'ad:e3:3e:a4:23:00'
     end
   end
+
+  describe '#other_mac' do
+    it 'gets the other MAC address' do
+      nf = Object.new
+      def nf.get reg
+        case reg
+        when 'MAC_RXTX_1_OTHER_MAC_HI_REG' then 0x0000a3a3
+        when 'MAC_RXTX_1_OTHER_MAC_LO_REG' then 0x45233400
+        end
+      end
+      port = NetFPGA::Port.new nf, 1
+      port.other_mac.must_equal 'a3:a3:45:23:34:00'
+    end
+  end
 end end
