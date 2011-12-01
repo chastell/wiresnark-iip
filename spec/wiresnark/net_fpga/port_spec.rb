499a952b1d90a180f6d8db34f7ae9826aca09ce5
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-01 11:13:08 +0100

    refactor NetFPGA::Port spec

--- spec/wiresnark/net_fpga/port_spec.rb
+++ spec/wiresnark/net_fpga/port_spec.rb
@@ -1,30 +1,29 @@
 require_relative '../../spec_helper'
 
 module Wiresnark describe NetFPGA::Port do
+  let :net_fpga do
+    net_fpga = Object.new
+    def net_fpga.get reg
+      case reg
+      when 'MAC_RXTX_0_LOCAL_MAC_HI_REG' then 0x0000ade3
+      when 'MAC_RXTX_0_LOCAL_MAC_LO_REG' then 0x3ea42300
+      when 'MAC_RXTX_1_OTHER_MAC_HI_REG' then 0x0000a3a3
+      when 'MAC_RXTX_1_OTHER_MAC_LO_REG' then 0x45233400
+      end
+    end
+    net_fpga
+  end
+
   describe '#local_mac' do
     it 'gets the local MAC address' do
-      nf = Object.new
-      def nf.get reg
-        case reg
-        when 'MAC_RXTX_0_LOCAL_MAC_HI_REG' then 0x0000ade3
-        when 'MAC_RXTX_0_LOCAL_MAC_LO_REG' then 0x3ea42300
-        end
-      end
-      port = NetFPGA::Port.new nf, 0
+      port = NetFPGA::Port.new net_fpga, 0
       port.local_mac.must_equal 'ad:e3:3e:a4:23:00'
     end
   end
 
   describe '#other_mac' do
     it 'gets the other MAC address' do
-      nf = Object.new
-      def nf.get reg
-        case reg
-        when 'MAC_RXTX_1_OTHER_MAC_HI_REG' then 0x0000a3a3
-        when 'MAC_RXTX_1_OTHER_MAC_LO_REG' then 0x45233400
-        end
-      end
-      port = NetFPGA::Port.new nf, 1
+      port = NetFPGA::Port.new net_fpga, 1
       port.other_mac.must_equal 'a3:a3:45:23:34:00'
     end
   end
