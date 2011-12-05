c58b407dd6dc1cd93d0d8edbdc3d8372ed63d5a1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-05 08:31:51 +0100

    drop NetFPGA#mac and #mac_set

--- lib/wiresnark/net_fpga.rb
+++ lib/wiresnark/net_fpga.rb
@@ -10,19 +10,6 @@ module Wiresnark class NetFPGA
     @regbridge.get @registers[register]
   end
 
-  def mac i, locality
-    hi = get "MAC_RXTX_#{i}_#{locality.upcase}_MAC_HI_REG"
-    lo = get "MAC_RXTX_#{i}_#{locality.upcase}_MAC_LO_REG"
-    ints = [(hi & 0xff00) >> 8, hi & 0xff, (lo & 0xff000000) >> 24, (lo & 0xff0000) >> 16, (lo & 0xff00) >> 8, lo & 0xff]
-    ints.map { |i| i.to_s(16).rjust 2, '0'}.join ':'
-  end
-
-  def mac_set i, locality, mac
-    ints = mac.split(':').map { |hex| hex.to_i 16 }
-    set "MAC_RXTX_#{i}_#{locality.upcase}_MAC_HI_REG", ints[0] << 8 | ints[1]
-    set "MAC_RXTX_#{i}_#{locality.upcase}_MAC_LO_REG", ints[2] << 24 | ints[3] << 16 | ints[4] << 8 | ints[5]
-  end
-
   def set register, value
     @regbridge.set @registers[register], value
   end
--- spec/wiresnark/net_fpga_spec.rb
+++ spec/wiresnark/net_fpga_spec.rb
@@ -13,34 +13,6 @@ module Wiresnark describe NetFPGA do
     end
   end
 
-  describe '#mac' do
-    it 'returns the MAC of the given interface' do
-      regbridge = Object.new
-      def regbridge.get reg
-        case reg
-        when 0x2000100 then 0x0000ade3
-        when 0x2000104 then 0x3ea42300
-        when 0x2000148 then 0x0000a3a3
-        when 0x200014c then 0x45233400
-        end
-      end
-      nf = NetFPGA.new 'spec/fixtures/reg_defines_simple_system_iip.h', regbridge
-      nf.mac(0, :local).must_equal 'ad:e3:3e:a4:23:00'
-      nf.mac(1, :other).must_equal 'a3:a3:45:23:34:00'
-    end
-  end
-
-  describe '#mac_set' do
-    it 'sets the MAC of the given interface' do
-      regbridge = MiniTest::Mock.new
-      regbridge.expect :set, nil, [0x2000100, 0x0000ade3]
-      regbridge.expect :set, nil, [0x2000104, 0x3ea42300]
-      nf = NetFPGA.new 'spec/fixtures/reg_defines_simple_system_iip.h', regbridge
-      nf.mac_set 0, :local, 'ad:e3:3e:a4:23:00'
-      regbridge.verify
-    end
-  end
-
   describe '#registers' do
     it 'returns the given NetFPGA’s register map' do
       nf = NetFPGA.new 'spec/fixtures/reg_defines_simple_system_iip.h'
