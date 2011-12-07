35ad1d9e10156d9add9ef51237b8271ae0bf5059
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-07 09:52:23 +0100

    NetFPGA::Port: rename @number to @port

--- lib/wiresnark/net_fpga/port.rb
+++ lib/wiresnark/net_fpga/port.rb
@@ -1,7 +1,7 @@
 module Wiresnark class NetFPGA::Port
-  def initialize net_fpga, number
+  def initialize net_fpga, port
     @net_fpga = net_fpga
-    @number   = number
+    @port     = port
   end
 
   def local_mac
@@ -13,11 +13,11 @@ module Wiresnark class NetFPGA::Port
   end
 
   def number_of_phases
-    @net_fpga.get "SCHEDULER_#{@number}_NUM_PHASES_REG"
+    @net_fpga.get "SCHEDULER_#{@port}_NUM_PHASES_REG"
   end
 
   def number_of_phases= number
-    @net_fpga.set "SCHEDULER_#{@number}_NUM_PHASES_REG", number
+    @net_fpga.set "SCHEDULER_#{@port}_NUM_PHASES_REG", number
   end
 
   def other_mac
@@ -31,15 +31,15 @@ module Wiresnark class NetFPGA::Port
   private
 
   def get_mac locality
-    hi = @net_fpga.get "MAC_RXTX_#{@number}_#{locality}_MAC_HI_REG"
-    lo = @net_fpga.get "MAC_RXTX_#{@number}_#{locality}_MAC_LO_REG"
+    hi = @net_fpga.get "MAC_RXTX_#{@port}_#{locality}_MAC_HI_REG"
+    lo = @net_fpga.get "MAC_RXTX_#{@port}_#{locality}_MAC_LO_REG"
     ints = [(hi & 0xff00) >> 8, hi & 0xff, (lo & 0xff000000) >> 24, (lo & 0xff0000) >> 16, (lo & 0xff00) >> 8, lo & 0xff]
     ints.map { |i| i.to_s(16).rjust 2, '0'}.join ':'
   end
 
   def set_mac mac, locality
     ints = mac.split(':').map { |hex| hex.to_i 16 }
-    @net_fpga.set "MAC_RXTX_#{@number}_#{locality}_MAC_HI_REG", ints[0] << 8 | ints[1]
-    @net_fpga.set "MAC_RXTX_#{@number}_#{locality}_MAC_LO_REG", ints[2] << 24 | ints[3] << 16 | ints[4] << 8 | ints[5]
+    @net_fpga.set "MAC_RXTX_#{@port}_#{locality}_MAC_HI_REG", ints[0] << 8 | ints[1]
+    @net_fpga.set "MAC_RXTX_#{@port}_#{locality}_MAC_LO_REG", ints[2] << 24 | ints[3] << 16 | ints[4] << 8 | ints[5]
   end
 end end
