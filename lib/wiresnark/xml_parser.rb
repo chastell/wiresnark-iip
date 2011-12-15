3aa30ace63ba5886de553a0d0669c03a3eddae8b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-15 09:45:55 +0100

    upgrade XMLParser to the new configuration format

--- lib/wiresnark/xml_parser.rb
+++ lib/wiresnark/xml_parser.rb
@@ -1,13 +1,10 @@
 module Wiresnark module XMLParser
   def self.parse path
     Hash[Nokogiri::XML(File.read path).xpath('/interfaces/interface').map do |interface|
-      local = interface.xpath('LinkLayerMACTransmitter/MACSourceAddress').first.text
-      other = interface.xpath('LinkLayerMACTransmitter/MACDestinationAddress').first.text
+      local = interface.xpath('v_port/MACSourceAddress').first.text
+      other = interface.xpath('v_port/MACDestinationAddress').first.text
 
-      phases = interface.xpath('SchedulerXENFPG/PhaseLength').map { |p| { type: p.attr('pi'), length: p.text.to_i } }
-      silent_length = interface.xpath('SchedulerXENFPG/Cyclelength').first.text.to_i - phases.map { |p| p[:length] }.inject(:+)
-      phases << { type: 'silent', length: silent_length } if silent_length > 0
-
+      phases = interface.xpath('Scheduler/PhaseLength').map { |p| { type: p.attr('pi'), length: p.text.to_i } }
       [interface.attr('name'), { local: local, other: other, phases: phases }]
     end]
   end
--- spec/wiresnark/xml_parser_spec.rb
+++ spec/wiresnark/xml_parser_spec.rb
@@ -4,13 +4,14 @@ module Wiresnark describe XMLParser do
   describe '.parse' do
     it 'parses the IIP XML config file' do
       hash = XMLParser.parse 'spec/fixtures/iip.conf.xml'
-      hash['eth0'][:local].must_equal 'ad:e3:3e:a4:23:00'
-      hash['eth1'][:other].must_equal 'a3:a3:45:23:34:00'
-      hash['eth2'][:phases].must_equal [
-        { type: 'QoS',    length: 300 },
-        { type: 'CAN',    length: 300 },
-        { type: 'MGT',    length: 300 },
-        { type: 'silent', length: 100 },
+      hash['eth0'][:local].must_equal 'ad:e3:3e:a4:23:aa'
+      hash['eth1'][:other].must_equal 'a3:aa:45:23:34:aa'
+      hash['eth0'][:phases].must_equal [
+        { type: 'QOS', length: 180 },
+        { type: 'CAN', length: 190 },
+        { type: 'DSS', length: 200 },
+        { type: 'MGT', length: 210 },
+        { type: 'NIL', length: 220 },
       ]
     end
   end
