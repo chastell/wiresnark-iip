1002b6ccecd7994fce5cb42896685152e4297ce8
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-28 13:28:39 +0100

    Shower#show: add CL and NP support

--- lib/wiresnark/shower.rb
+++ lib/wiresnark/shower.rb
@@ -7,8 +7,10 @@ module Wiresnark class Shower
     port = opts['interface'][/\d$/].to_i
 
     case opts['param']
+    when 'CL'    then @net_fpga.ports[port].phases.map { |p| p[:length] }.inject :+
     when 'MACDA' then @net_fpga.ports[port].other_mac
     when 'MACSA' then @net_fpga.ports[port].local_mac
+    when 'NP'    then @net_fpga.ports[port].phases.size
     end
   end
 end end
--- spec/wiresnark/shower_spec.rb
+++ spec/wiresnark/shower_spec.rb
@@ -3,18 +3,24 @@ require_relative '../spec_helper'
 module Wiresnark describe Shower do
   describe '#show' do
     it 'prints the proper NetFPGA params' do
-      port0 = MiniTest::Mock.new
-      port1 = MiniTest::Mock.new
-      nf    = MiniTest::Mock.new
-      port0.expect :local_mac, '00:11:22:33:44:55'
-      port1.expect :other_mac, 'aa:bb:cc:dd:ee:ff'
-      nf.expect :ports, [port0, port1]
+      p0 = MiniTest::Mock.new
+      p1 = MiniTest::Mock.new
+      p2 = MiniTest::Mock.new
+      p3 = MiniTest::Mock.new
+      nf = MiniTest::Mock.new
+      p0.expect :local_mac, '00:11:22:33:44:55'
+      p1.expect :other_mac, 'aa:bb:cc:dd:ee:ff'
+      p2.expect :phases, [{ type: 'QOS', length: 100 }, { type: 'CAN', length: 200 }, { type: 'MGT', length: 300 }, { type: 'NIL', length: 400 }]
+      nf.expect :ports, [p0, p1, p2]
 
       Shower.new(nf).show({ 'interface' => 'eth0', 'param' => 'MACSA', 'vport' => 'v_1' }).must_equal '00:11:22:33:44:55'
       Shower.new(nf).show({ 'interface' => 'eth1', 'param' => 'MACDA', 'vport' => 'v_1' }).must_equal 'aa:bb:cc:dd:ee:ff'
+      Shower.new(nf).show({ 'interface' => 'eth2', 'param' => 'CL',    'vport' => 'v_1' }).must_equal 1000
+      Shower.new(nf).show({ 'interface' => 'eth2', 'param' => 'NP',    'vport' => 'v_1' }).must_equal 4
 
-      port0.verify
-      port1.verify
+      p0.verify
+      p1.verify
+      p2.verify
       nf.verify
     end
   end
