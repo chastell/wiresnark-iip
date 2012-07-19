f1d0227babe78804cb46b50c1f1d3932db55143d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-07-19 21:18:51 +0200

    NetFPGA spec: adjust for new register addresses

--- spec/wiresnark/iip/net_fpga_spec.rb
+++ spec/wiresnark/iip/net_fpga_spec.rb
@@ -31,52 +31,52 @@ module Wiresnark::IIP describe NetFPGA do
       regbridge = MiniTest::Mock.new
 
       # port 0, EtherType
-      regbridge.expect :set_register, nil, [0x2000110, 0xabcd]
+      regbridge.expect :set_register, nil, [0x2000010, 0xabcd]
 
       # port 0, local MAC
-      regbridge.expect :set_register, nil, [0x2000100, 0x0000ade3]
-      regbridge.expect :set_register, nil, [0x2000104, 0x3ea423aa]
+      regbridge.expect :set_register, nil, [0x2000000, 0x0000ade3]
+      regbridge.expect :set_register, nil, [0x2000004, 0x3ea423aa]
 
       # port 0, other MAC
-      regbridge.expect :set_register, nil, [0x2000108, 0x0000a3a3]
-      regbridge.expect :set_register, nil, [0x200010c, 0x452334aa]
+      regbridge.expect :set_register, nil, [0x2000008, 0x0000a3a3]
+      regbridge.expect :set_register, nil, [0x200000c, 0x452334aa]
 
       # port 0, number of phases
-      regbridge.expect :set_register, nil, [0x200012c, 5]
+      regbridge.expect :set_register, nil, [0x200002c, 5]
 
       # port 0, phase types + lenghts
-      regbridge.expect :set_register, nil, [0x2000150, 1]
-      regbridge.expect :set_register, nil, [0x2000130, 23]
+      regbridge.expect :set_register, nil, [0x2000050, 1]
+      regbridge.expect :set_register, nil, [0x2000030, 23]
 
-      regbridge.expect :set_register, nil, [0x2000154, 2]
-      regbridge.expect :set_register, nil, [0x2000134, 24]
+      regbridge.expect :set_register, nil, [0x2000054, 2]
+      regbridge.expect :set_register, nil, [0x2000034, 24]
 
-      regbridge.expect :set_register, nil, [0x2000158, 3]
-      regbridge.expect :set_register, nil, [0x2000138, 25]
+      regbridge.expect :set_register, nil, [0x2000058, 3]
+      regbridge.expect :set_register, nil, [0x2000038, 25]
 
-      regbridge.expect :set_register, nil, [0x200015c, 4]
-      regbridge.expect :set_register, nil, [0x200013c, 26]
+      regbridge.expect :set_register, nil, [0x200005c, 4]
+      regbridge.expect :set_register, nil, [0x200003c, 26]
 
-      regbridge.expect :set_register, nil, [0x2000160, 0]
-      regbridge.expect :set_register, nil, [0x2000140, 27]
+      regbridge.expect :set_register, nil, [0x2000060, 0]
+      regbridge.expect :set_register, nil, [0x2000040, 27]
 
       # port 0, type map
-      regbridge.expect :set_register, nil, [0x2000120, 2]
-      regbridge.expect :set_register, nil, [0x2000128, 7]
+      regbridge.expect :set_register, nil, [0x2000020, 2]
+      regbridge.expect :set_register, nil, [0x2000028, 7]
 
       # port 1, EtherType
-      regbridge.expect :set_register, nil, [0x2000210, 0]
+      regbridge.expect :set_register, nil, [0x2000110, 0]
 
       # port 1, local MAC
-      regbridge.expect :set_register, nil, [0x2000200, 0x0000ade3]
-      regbridge.expect :set_register, nil, [0x2000204, 0x3eb423aa]
+      regbridge.expect :set_register, nil, [0x2000100, 0x0000ade3]
+      regbridge.expect :set_register, nil, [0x2000104, 0x3eb423aa]
 
       # port 1, other MAC
-      regbridge.expect :set_register, nil, [0x2000208, 0x0000a3aa]
-      regbridge.expect :set_register, nil, [0x200020c, 0x452334aa]
+      regbridge.expect :set_register, nil, [0x2000108, 0x0000a3aa]
+      regbridge.expect :set_register, nil, [0x200010c, 0x452334aa]
 
       # port 1, number of phases
-      regbridge.expect :set_register, nil, [0x200022c, 0]
+      regbridge.expect :set_register, nil, [0x200012c, 0]
 
       nf = NetFPGA.new regbridge
       nf.config = config
@@ -88,7 +88,7 @@ module Wiresnark::IIP describe NetFPGA do
   describe '#get' do
     it 'gets the register contents (via the passed bridge)' do
       regbridge = MiniTest::Mock.new
-      regbridge.expect :get_register, 7, [0x200012c]
+      regbridge.expect :get_register, 7, [0x200002c]
       nf = NetFPGA.new regbridge
       nf.get('PORT_0_NUM_PHASES_REG').must_equal 7
       regbridge.verify
@@ -106,14 +106,14 @@ module Wiresnark::IIP describe NetFPGA do
   describe '#registers' do
     it 'returns the given NetFPGA’s register map' do
       nf = NetFPGA.new
-      nf.registers['PORT_0_NUM_PHASES_REG'].must_equal 0x200012c
+      nf.registers['PORT_0_NUM_PHASES_REG'].must_equal 0x200002c
     end
   end
 
   describe '#set' do
     it 'sets the register contents (via the passed bridge)' do
       regbridge = MiniTest::Mock.new
-      regbridge.expect :set_register, nil, [0x200012c, 7]
+      regbridge.expect :set_register, nil, [0x200002c, 7]
       nf = NetFPGA.new regbridge
       nf.set 'PORT_0_NUM_PHASES_REG', 7
       regbridge.verify
