5f3deeeb7f014d972fa8dc3eb76680de6ac5c483
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-10-17 09:31:58 +0200

    Executable: prefer attr_readers to ivar access

--- lib/wiresnark/iip/executable.rb
+++ lib/wiresnark/iip/executable.rb
@@ -5,14 +5,18 @@ module Wiresnark::IIP class Executable
   end
 
   def run opts = { committer: Committer.new, getter: Getter.new, shower: Shower.new }
-    case @command
+    case command
     when 'commit'
-      ConfigParser.new(@args.first).warnings.each { |warning| warn warning }
-      opts[:committer].commit @args.first
+      ConfigParser.new(args.first).warnings.each { |warning| warn warning }
+      opts[:committer].commit args.first
     when 'get'
       puts opts[:getter].get
     when 'show'
-      puts opts[:shower].show @args
+      puts opts[:shower].show args
     end
   end
+
+  private
+
+  attr_reader :args, :command
 end end
