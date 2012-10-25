70d5df7b079a21c7383f26cc262bb82660eebf28
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-10-25 06:47:01 +0200

    require files explicitly

--- lib/wiresnark/iip.rb
+++ lib/wiresnark/iip.rb
@@ -4,4 +4,12 @@ require 'nokogiri'
 module Wiresnark module IIP
 end end
 
-Dir["#{File.dirname(__FILE__)}/**/*.rb"].sort.each { |f| require f }
+require_relative 'iip/committer'
+require_relative 'iip/config_parser'
+require_relative 'iip/executable'
+require_relative 'iip/getter'
+require_relative 'iip/net_fpga'
+require_relative 'iip/net_fpga/bridge'
+require_relative 'iip/net_fpga/port'
+require_relative 'iip/reg_parser'
+require_relative 'iip/shower'
